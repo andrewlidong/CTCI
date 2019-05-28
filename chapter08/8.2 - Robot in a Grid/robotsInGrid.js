4708967b7209dad5d5f31a07710b143a154f57ee
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-28 14:27:16 -0400

    Modified robots in grid

--- chapter08/8.2 - Robot in a Grid/robotsInGrid.js
+++ chapter08/8.2 - Robot in a Grid/robotsInGrid.js
@@ -2,19 +2,21 @@ function findPaths(grid) {
     let paths = [];
     let lastRow = grid.length - 1;
     let lastCol = grid[0].length - 1;
-    let recurse = function(row, col, currentPath) {
+
+    const traverse = function(row, col, currentPath) {
         if (row === lastRow && col === lastCol) {
             paths.push(currentPath.concat([row, col]));
         } else if (row <= lastRow && col <= lastCol) {
             if (row < lastRow && grid[row + 1][col] !== 'x') {
-                recurse(row + 1, col, currentPath.concat([[row, col]]));
+                traverse(row + 1, col, currentPath.concat([[row, col]]));
             }
             if (col , lastCol && grid[row][col + 1] !== 'x') {
-                recurse(row, col + 1, currentPath.concat([[row, col]]));
+                traverse(row, col + 1, currentPath.concat([[row, col]]));
             }
         }
     };
-    recurse(0,0,[]);
+    
+    traverse(0,0,[]);
     return paths;
 }
 
