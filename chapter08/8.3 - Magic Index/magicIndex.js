3fcb62a85c19b87e675cc0f09f7cac48dcb011cb
Andrew Li Dong <andrewdong1994@gmail.com>
2019-05-28 16:34:56 -0400

    Magic Index Finished

--- chapter08/8.3 - Magic Index/magicIndex.js
+++ chapter08/8.3 - Magic Index/magicIndex.js
@@ -37,6 +37,29 @@ function magicFast(arr, start = 0, end = arr.length - 1) {
 //     }
 // }
 
+function magicFastUnsorted(arr, start = 0, end = arr.length - 1) {
+    if (end < start) return -1;
+
+    let midIndex = Math.floor((start + end) / 2);
+    let midValue = arr[midIndex];
+    if (midValue === midIndex) {
+        return midIndex;
+    }
+
+    // Search left
+    let leftIndex = Math.min(midIndex - 1, midValue);
+    let left = magicFastUnsorted(arr, start, leftIndex);
+    if (left >= 0) {
+        return left;
+    }
+
+    // Search right
+    let rightIndex = Math.max(midIndex + 1, midValue);
+    let right = magicFastUnsorted(arr, rightIndex, end);
+
+    return right;
+}
+ 
 console.log(magicSlow([-1, 0, 1, 3, 9, 100]), 3);
 console.log(magicSlow([-1, 0, 1, 2, 3, 5, 100, 200, 300, 400, 500, 600, 700]), 5);
 console.log(magicSlow([5, 5, 5, 5, 5, 5]), 5); // would need to be in linear runtime if values are not distinct
@@ -44,3 +67,7 @@ console.log(magicSlow([5, 5, 5, 5, 5, 5]), 5); // would need to be in linear run
 console.log(magicFast([-1, 0, 1, 3, 9, 100]), 3);
 console.log(magicFast([-1, 0, 1, 2, 3, 5, 100, 200, 300, 400, 500, 600, 700]), 5);
 console.log(magicFast([5, 5, 5, 5, 5, 5]), 5); // would need to be in linear runtime if values are not distinct
+
+console.log(magicFastUnsorted([-1, 0, 1, 3, 9, 100]), 3);
+console.log(magicFastUnsorted([-1, 0, 1, 2, 3, 5, 100, 200, 300, 400, 500, 600, 700]), 5);
+console.log(magicFastUnsorted([5, 5, 5, 5, 5, 5]), 5); // would need to be in linear runtime if values are not distinct
